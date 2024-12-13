a4b607c3e5c312a75aff987fd9515a2f947f7dbe
DouglasDG12 <douglas82108@gmail.com>
2024-12-13 17:44:04 -0400

    atualizacao no sorteador

--- scripts.js
+++ scripts.js
@@ -2,8 +2,14 @@ function sorteador() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    if (min >= max) {
+        alert("O valor maximo não pode ser menor ou igual o valor minimo!!")
+    } else{
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+        alert(result)
+    }
 
 }
+
